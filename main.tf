7ebc42c626602638b33608f1dc2d78a2db83d570
ImuraEiki <imura2005@gmail.com>
2025-08-03 17:58:48 +0900

    feat: リモートステートの設定

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.12.2"
+  backend "s3" {
+  bucket         = "terrafom-state-s3"
+  key            = "terraform/terraform.tfstate"
+  region         = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
